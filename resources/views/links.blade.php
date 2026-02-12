3eed7d5e74bb182c28b1ab12eedd16b13152d27b
cleven <cleven.noreplay@gmail.com>
2026-02-12 20:18:13 +0300

    refact: ...

--- resources/views/links.blade.php
+++
@@ -1,9 +0,0 @@
- <!-- for title img -->
- <link rel="shortcut icon" type="../image/icon" href="../img/logo.jpg" />
- <link rel="stylesheet" href="{{ asset('css/style.css') }}">
- <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
- <link rel="stylesheet" href="{{ asset('fontawesome/css/all.min.css') }}">
- <script src="{{ asset('jquery/js/jquery-3.5.1.js') }}"></script>
- <link rel="stylesheet" href="{{ asset('jquery/css/dataTables.bootstrap5.min.css') }}">
- <script src="{{ asset('jquery/js/dataTables.bootstrap5.min.js') }}"></script>
- <script src="{{ asset('jquery/js/jquery.dataTables.min.js') }}"></script>
--- resources/views/user/discusion-forum.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.user')
-@section('content')
-    <div class="container mt-5 ">
-        <h1 class="text text-primary"><strong class="text-warning">Welcome</strong>, {{ $userInfo->fullname }}</h1>
-        <p>we're kindly request you to <a class="text-decoration-none fw-bold " href="">click here </a> to access the full club discussion forum</p>
-    </div>
-@endsection
